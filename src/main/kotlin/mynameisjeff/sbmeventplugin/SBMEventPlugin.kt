5503b7a47c09b8693e0fdf4966ca5702f354bf9a
My-Name-Is-Jeff <37018278+My-Name-Is-Jeff@users.noreply.github.com>
2023-03-06 18:47:04 -0500

    auto respawn

--- src/main/kotlin/mynameisjeff/sbmeventplugin/SBMEventPlugin.kt
+++ src/main/kotlin/mynameisjeff/sbmeventplugin/SBMEventPlugin.kt
@@ -1,5 +1,6 @@
 package mynameisjeff.sbmeventplugin
 
+import mynameisjeff.sbmeventplugin.game.loadAutoRespawn
 import mynameisjeff.sbmeventplugin.game.loadNearestPlayerTracking
 import mynameisjeff.sbmeventplugin.game.loadSpectatorLock
 import mynameisjeff.sbmeventplugin.game.loadTimer
@@ -7,6 +8,7 @@ import net.axay.kspigot.main.KSpigot
 
 class SBMEventPlugin : KSpigot() {
     override fun startup() {
+        loadAutoRespawn()
         loadNearestPlayerTracking()
         loadSpectatorLock()
         loadTimer()
---
+++ src/main/kotlin/mynameisjeff/sbmeventplugin/game/autoRespawn.kt
@@ -0,0 +1,15 @@
+package mynameisjeff.sbmeventplugin.game
+
+import net.axay.kspigot.event.listen
+import net.axay.kspigot.runnables.taskRunLater
+import net.minecraft.network.protocol.game.ServerboundClientCommandPacket
+import org.bukkit.craftbukkit.v1_18_R2.entity.CraftPlayer
+import org.bukkit.event.entity.PlayerDeathEvent
+
+fun loadAutoRespawn() {
+    listen<PlayerDeathEvent> {
+        taskRunLater(1) {
+            (it.player as CraftPlayer).handle.connection.handleClientCommand(ServerboundClientCommandPacket(ServerboundClientCommandPacket.Action.PERFORM_RESPAWN))
+        }
+    }
+}
